c04e9ffd08a1a930e820ece232adf5bd3d9f5bf4
John Hammond <JPHammonds@anl.gov>
2015-04-02 15:27:10 -0500

    Point at asyn-4-26 instead of 4-25.

--- iocs/PICamIOC/iocBoot/iocPICam/start_epics-displays.bat
+++ iocs/PICamIOC/iocBoot/iocPICam/start_epics-displays.bat
@@ -1,6 +1,6 @@
 set SUPPORT=C:\PICAM\synApps_5_7\support
-set AREA_DETECTOR=%SUPPORT%\areaDetector_2_git
-set ASYN=%SUPPORT%\asyn-4-25
+set AREA_DETECTOR=%SUPPORT%\areaDetector-2-x-git
+set ASYN=%SUPPORT%\asyn-4-26
 set EPICS_DISPLAY_PATH=%AREA_DETECTOR%\ADPICam\PICamApp\op\adl;%AREA_DETECTOR%\ADCore\ADApp\op\adl;%ASYN%\opi\medm
 echo %EPICS_DISPLAY_PATH%
 start medm -x -macro "P=13PICAM1:, R=cam1:" PICAMBase.adl
