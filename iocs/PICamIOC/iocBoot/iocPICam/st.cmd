b4c692c759a63b95be13f0514b223dcf06cbc5a2
John Hammond <JPHammonds@anl.gov>
2015-04-02 15:26:29 -0500

    Add in CBUFFS & EPICS_DB_INCLUDE_PATH definitions.

--- iocs/PICamIOC/iocBoot/iocPICam/st.cmd
+++ iocs/PICamIOC/iocBoot/iocPICam/st.cmd
@@ -5,12 +5,22 @@ errlogInit(20000)
 dbLoadDatabase("$(TOP)/dbd/PICamApp.dbd")
 PICamApp_registerRecordDeviceDriver(pdbbase) 
 
+# Prefix for all records
 epicsEnvSet("PREFIX", "13PICAM1:")
+# The port name for the detector
 epicsEnvSet("PORT",   "PICAMDET1")
+# The queue size for all plugins
 epicsEnvSet("QSIZE",  "20")
+# The maximim image width; used for row profiles in the NDPluginStats plugin
 epicsEnvSet("XSIZE",  "2048")
+# The maximim image height; used for column profiles in the NDPluginStats plugin
 epicsEnvSet("YSIZE",  "2048")
+# The maximum number of time seried points in the NDPluginStats plugin
 epicsEnvSet("NCHANS", "2048")
+# The maximum number of frames buffered in the NDPluginCircularBuff plugin
+epicsEnvSet("CBUFFS", "500")
+# The search path for database files
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 # Create a PICam driver
 # PICamConfig(const char *portName, IDType, IDValue, maxBuffers, size_t maxMemory, int priority, int stackSize)
