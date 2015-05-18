eca9bf2939ee2793f93c383a90469864812c3371
John Hammond <JPHammonds@anl.gov>
2015-05-18 16:19:08 -0500

    Update to use synApps 5.8.

--- iocs/PICamIOC/iocBoot/iocPICam/start_epics-displays.bat
+++ iocs/PICamIOC/iocBoot/iocPICam/start_epics-displays.bat
@@ -1,6 +1,6 @@
-set SUPPORT=C:\PICAM\synApps_5_7\support
-set AREA_DETECTOR=%SUPPORT%\areaDetector-2-x-git
+set SUPPORT=C:\PICAM\synApps_5_8\support
+set AREA_DETECTOR=%SUPPORT%\areaDetector-R2-0
 set ASYN=%SUPPORT%\asyn-4-26
-set EPICS_DISPLAY_PATH=%AREA_DETECTOR%\ADPICam\PICamApp\op\adl;%AREA_DETECTOR%\ADCore\ADApp\op\adl;%ASYN%\opi\medm
+set EPICS_DISPLAY_PATH=%AREA_DETECTOR%\ADPICam\PICamApp\op\adl;%AREA_DETECTOR%\ADCore-R2-2\ADApp\op\adl;%ASYN%\opi\medm
 echo %EPICS_DISPLAY_PATH%
 start medm -x -macro "P=13PICAM1:, R=cam1:" PICAMBase.adl
